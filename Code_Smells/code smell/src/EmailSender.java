00ea01f1a8fc0ba4abf4f0735f19230717edc3f0
Nithyaa <nithyaa.hyma@gmail.com>
2024-11-01 17:22:50 -0400

    worked on the diningActivity

--- Code_Smells/code smell/src/EmailSender.java
+++ Code_Smells/code smell/src/EmailSender.java
@@ -1,12 +1,16 @@
-public class EmailSender {
-    // Private constructor to prevent instantiation
-    private EmailSender() {
-        throw new UnsupportedOperationException("This is a utility class and cannot be instantiated");
-    }
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-    public static void sendEmail(String customerEmail, String subject, String message) {
-        System.out.println("Email to: " + customerEmail);
-        System.out.println("Subject: " + subject);
-        System.out.println("Body: " + message);
-    }
+public class EmailSender {
+   private static final Logger logger = Logger.getLogger(EmailSender.class.getName());
+   // Private constructor to prevent instantiation
+   private EmailSender() {
+      throw new UnsupportedOperationException("This is a utility class and cannot be instantiated");
+   }
+   //logging instead of system.out/system.err
+   public static void sendEmail(String customerEmail, String subject, String message) {
+      logger.log(Level.INFO, "Email to: {0}", customerEmail);
+      logger.log(Level.INFO, "Subject: {0}", subject);
+      logger.log(Level.INFO, "Body: {0}", message);
+   }
 }
--- Project/app/src/main/java/com/example/wandersyncteam10/view/DiningReservationManager.java
+++ Project/app/src/main/java/com/example/wandersyncteam10/view/DiningReservationManager.java
@@ -1,14 +1,15 @@
 package com.example.wandersyncteam10.view;
 
 import android.util.Log;
-
 import androidx.annotation.NonNull;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.HashMap;
@@ -30,18 +31,31 @@ public class DiningReservationManager {
 
         db.collection("diningReservations")
                 .add(reservation)
-                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-                    @Override
-                    public void onSuccess(DocumentReference documentReference) {
-                        Log.d("Firebase", "DocumentSnapshot added with ID: " + documentReference.getId());
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Log.w("Firebase", "Error adding document", e);
+                .addOnSuccessListener(documentReference -> Log.d("Firebase", "DocumentSnapshot added with ID: " + documentReference.getId()))
+                .addOnFailureListener(e -> Log.w("Firebase", "Error adding document", e));
+    }
+
+    public void fetchDiningReservations(final ReservationFetchListener listener) {
+        db.collection("diningReservations")
+                .get()
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        ArrayList<Reservation> reservations = new ArrayList<>();
+                        for (QueryDocumentSnapshot document : task.getResult()) {
+                            String location = document.getString("location");
+                            String website = document.getString("website");
+                            String time = document.getDate("reservationTime").toString();
+                            reservations.add(new Reservation(location, website, time));
+                        }
+                        listener.onFetchComplete(reservations);
+                    } else {
+                        listener.onFetchFailed(task.getException());
                     }
                 });
     }
-}
 
+    public interface ReservationFetchListener {
+        void onFetchComplete(ArrayList<Reservation> reservations);
+        void onFetchFailed(Exception e);
+    }
+}
