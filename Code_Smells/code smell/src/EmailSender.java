8f82c918a0b21bf9504b9ffda4bca95ae35699d3
RoshniDaruvuri <roshni11.daruvuri@gmail.com>
2024-11-01 14:23:27 -0400

    finished one code smells about OO abuser in EmailSender

--- Code_Smells/code smell/src/EmailSender.java
+++ Code_Smells/code smell/src/EmailSender.java
@@ -1,6 +1,10 @@
-
 public class EmailSender {
-    public static void sendEmail(String customerEmail, String subject, String message){
+    // Private constructor to prevent instantiation
+    private EmailSender() {
+        throw new UnsupportedOperationException("This is a utility class and cannot be instantiated");
+    }
+
+    public static void sendEmail(String customerEmail, String subject, String message) {
         System.out.println("Email to: " + customerEmail);
         System.out.println("Subject: " + subject);
         System.out.println("Body: " + message);
