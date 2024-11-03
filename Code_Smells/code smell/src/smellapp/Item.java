ef2bdead36303708811c103e4309baa7a8c0bee2
Aditi Tripathi <139368333+aaditrip@users.noreply.github.com>
2024-11-03 17:05:37 -0500

    Update Item.java

--- Code_Smells/code smell/src/smellapp/Item.java
+++ Code_Smells/code smell/src/smellapp/Item.java
@@ -7,12 +7,12 @@ class Item {
     private DiscountType discountType;
     private double discountAmount;
 
-    public Item(String name, double price, int quantity, DiscountType discountType, double discountAmount) {
+    public Item(String name, double price, int quantity) { 
         this.name = name;
         this.price = price;
         this.quantity = quantity;
-        this.discountType = discountType;
-        this.discountAmount = discountAmount;
+        this.discountType = null;
+        this.discountAmount = 0.0;
     }
 
     public String getName() {
@@ -34,4 +34,17 @@ class Item {
     public double getDiscountAmount() {
         return discountAmount;
     }
+
+    //setter to set the discount type.
+    public void setDiscountType(DiscountType discountType){
+        this.discountType = discountType;
+    }
+
+    //setter to set the discount amount.
+    public void setDiscountAmount(double discountAmount){
+        this.discountAmount = discountAmount;
+    }
+
+
 }
+
