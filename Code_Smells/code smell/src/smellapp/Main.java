e03d24552543c02af1c62aa249c5f9c9ea581cd9
Aditi Tripathi <139368333+aaditrip@users.noreply.github.com>
2024-11-03 13:11:50 -0500

    Update Main.java

--- Code_Smells/code smell/src/smellapp/Main.java
+++ Code_Smells/code smell/src/smellapp/Main.java
@@ -21,7 +21,6 @@ public class Main {
 
         Order order = new Order(items, "John Doe", "johndoe@example.com");
 
-        //System.out.println("Total Price: " + order.calculateTotalPrice());
         logger.info("Total Price: " + order.calculateTotalPrice());
 
         order.sendConfirmationEmail();
